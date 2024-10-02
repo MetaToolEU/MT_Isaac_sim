462c0f211342f19cbc4fa0fcf32ee1f3a2847f4f
Mohamed khalil kahlaoui <khalil.kahlaoui@gmail.com>
2024-10-02 12:23:58 +0200

    Update start_docker.sh

--- Docker/start_docker.sh
+++ Docker/start_docker.sh
@@ -72,7 +72,6 @@ elif [[ "$input_arg" == *isaac_sim* ]] ; then
         -v ~/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
         -v ~/docker/isaac-sim/documents:/home/$USER/Documents:rw \
         -v "$SCRIPT_DIR../../Isaac_launch/:/pkgs/curobo/examples/Isaac_launch:rw"\
-        -v "$SCRIPT_DIR../../usd_files/:/pkgs/curobo/src/curobo/content/assets/usd_files:rw"\
         --volume /dev:/dev \
         curobo_docker:$input_arg
 
